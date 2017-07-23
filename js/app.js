74e0c47d42ad69a20cea68ac77c6b348129960fd
nicoconi <nico.roillet@gmail.com>
2017-07-23 21:52:15 +0200

    sdf

---
+++ js/app.js
@@ -0,0 +1,7 @@
+    // create the module and name it scotchApp
+    var gameApp = angular.module('gameApp', []);
+
+    // create the controller and inject Angular's $scope
+    gameApp.controller('mainController', function($scope) {
+
+      });
